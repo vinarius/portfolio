3953ba3559235e47dea8b6a1262117c911570100
Mark <vindevccm@gmail.com>
2020-02-17 08:48:01 -0500

    changes testing ec2

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const fs = require('fs');
 
 const serverPort = process.env.PORT || 8080;
 const app = express();
 
 app.get('*', (req, res) => {
-    res.send(__dirname);
+    const obj = {
+        '__dirname': fs.readdirSync(__dirname),
+        'Interface': fs.readdirSync(__dirname + '/Interface'),
+        'dist': fs.readdirSync(__dirname + '/Interface/dist'),
+        'Portfolio': fs.readdirSync(__dirname + '/Interface/dist/Portfolio')
+    };
+    res.send(obj);
     // res.sendFile(__dirname + '/Interface/dist/Portfolio/index.html');
 });
 // app.use(express.static(__dirname + '/Interface/dist/Portfolio'));
