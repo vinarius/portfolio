b485c4c62a0833431e738913e7462f64b74e6166
Mark <vindevccm@gmail.com>
2020-02-17 10:21:10 -0500

    Reset buildspec and server

--- server.js
+++ server.js
@@ -4,18 +4,11 @@ const fs = require('fs');
 const serverPort = process.env.PORT || 8080;
 const app = express();
 
-app.get('*', (req, res) => {
-    const obj = {
-        '__dirname': fs.readdirSync(__dirname),
-        'Interface': fs.readdirSync(__dirname + '/Interface')
-        // 'dist': fs.readdirSync(__dirname + '/Interface/dist'),
-        // 'Portfolio': fs.readdirSync(__dirname + '/Interface/dist/Portfolio')
-    };
-    res.send(obj);
-    // res.sendFile(__dirname + '/Interface/dist/Portfolio/index.html');
-});
-// app.use(express.static(__dirname + '/Interface/dist/Portfolio'));
+app.use(express.static(__dirname + '/Interface/dist/Portfolio'));
 
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/Interface/dist/Portfolio/index.html');
+});
 
 app.listen(serverPort, (err) => {
     if(err) throw err;
