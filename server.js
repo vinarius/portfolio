9865c56bcfbc1852b3134c9fbee30d1168781803
Mark <vindevccm@gmail.com>
2020-02-16 21:11:11 -0500

    Restructured server files

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const serverPort = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/../Interface/dist/Portfolio'));
+app.use(express.static(__dirname + '/Interface/dist/Portfolio'));
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/../Interface/dist/Portfolio/index.html');
+    res.sendFile(__dirname + '/Interface/dist/Portfolio/index.html');
 });
 
 app.listen(serverPort, (err) => {
