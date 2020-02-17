968b7b398692b45af72b86ffbad97b6e39aae3ed
Mark <vindevccm@gmail.com>
2020-02-17 08:39:34 -0500

    changes testing ec2

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const serverPort = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/Interface/dist/Portfolio'));
-
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/Interface/dist/Portfolio/index.html');
+    res.send(__dirname);
+    // res.sendFile(__dirname + '/Interface/dist/Portfolio/index.html');
 });
+// app.use(express.static(__dirname + '/Interface/dist/Portfolio'));
+
 
 app.listen(serverPort, (err) => {
     if(err) throw err;
