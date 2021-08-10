e45f4ec281333aac54bc998e8369280cf527f49f
Mark Kraus <vindevccm@gmail.com>
2021-08-09 22:46:13 -0400

    added basic tailwind config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import React, {ReactElement} from 'react';
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home(): ReactElement {
   return (
     <div className={styles.container}>
       <Head>
