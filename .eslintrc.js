b8b4f625f25a9975d1c390f570552163e49131ab
vinarius <vindevccm@gmail.com>
2021-08-11 16:09:58 -0400

    brought in typescript-eslint - new linting rules. overlay - navbar.

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next",
+    "next/core-web-vitals"
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@next/next/no-img-element": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: false,
+      },
+    ],
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    curly: "error",
+    indent: ["error", 2, { SwitchCase: 1 }],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "no-cond-assign": ["error", "always"],
+    "init-declarations": "off",
+    "no-inline-comments": "off",
+    eqeqeq: "warn",
+    "comma-dangle": "error"
+  }
+};
