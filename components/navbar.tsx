16a8d3a109cae1d13ec4155a164d36f7b8ec6c64
vinarius <vindevccm@gmail.com>
2021-08-18 12:33:05 -0400

    navbar styling, added placeholder content on main page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,69 +1,67 @@
-import { Disclosure, Menu } from '@headlessui/react';
+import { Disclosure } from '@headlessui/react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
-import React, { ReactElement, useContext } from 'react';
+import React, { ReactElement, useContext, useState } from 'react';
 
 import { STATEACTIONS } from '../context/stateActions';
 import { myContext } from '../context/stateProvider';
-
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false }
-];
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
 
 export default function Navbar(): ReactElement {
-  const {state, dispatch} = useContext(myContext);
-  const {isOverlayOpen} = state;
+  const { state, dispatch } = useContext(myContext);
+  const { isOverlayOpen } = state;
+
+  const [navigation, setNavigation] = useState([
+    { name: 'About Me', href: '#', current: true },
+    { name: 'Portfolio', href: '#', current: false },
+    { name: 'Contact Me', href: '#', current: false }
+  ]);
 
   const handleOpenOverlay = () => {
-    console.log('fired');
     if (isOverlayOpen) {
-      dispatch({type: STATEACTIONS.closeOverlay});
+      dispatch({ type: STATEACTIONS.closeOverlay });
     } else {
-      dispatch({type: STATEACTIONS.openOverlay});
+      dispatch({ type: STATEACTIONS.openOverlay });
     }
   };
+
+  function setNavigationCurrent(index: number): void {
+    setNavigation(
+      navigation.map((val, ix) => {
+        val.current = index === ix;
+        return val;
+      })
+    );
+  }
 
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8"
-            onClick={handleOpenOverlay}
-          >
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="absolute left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white transition-all">
                   <span className="sr-only">Open main menu</span>
+                  
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
                     <MenuIcon className="block h-6 w-6" aria-hidden="true" />
                   )}
+
+                  <div className="flex-shrink-0 flex items-center">
+                    <p className="text-white text-lg sm:text-2xl ml-3 sm:ml-0">Mark Kraus</p>
+                  </div>
                 </Disclosure.Button>
               </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center">
-                  <img
-                    className="block lg:hidden h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                    alt="Workflow"
-                  />
-                  <img
-                    className="hidden lg:block h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                    alt="Workflow"
-                  />
-                </div>
+              <div className="flex-1 flex items-center justify-start sm:items-stretch">
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
-                    {navigation.map((item) => (
+                    {navigation.map((item, index) => (
                       <a
                         key={item.name}
                         href={item.href}
@@ -72,6 +70,7 @@ export default function Navbar(): ReactElement {
                           'px-3 py-2 rounded-md text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
+                        onClick={() => setNavigationCurrent(index)}
                       >
                         {item.name}
                       </a>
@@ -80,73 +79,23 @@ export default function Navbar(): ReactElement {
                 </div>
               </div>
               <div
-                className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+                className="absolute right-0 flex items-center mr-5 sm:static sm:inset-auto sm:pr-0 cursor-pointer rounded-md text-lg text-white hover:bg-gray-300 hover:text-black transition-all"
+                onClick={handleOpenOverlay}
               >
-
-                {/* Profile dropdown */}
-                <Menu as="div" className="ml-3 relative">
-                  <div>
-                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        // src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        src="/mark-portrait.jpg"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  {/* <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition> */}
-                </Menu>
+                <p className="mx-4">My Skills</p>
+                <img
+                  className="h-10 w-10 rounded-full mr-2"
+                  src="/mark-portrait.jpg"
+                  alt="image of a man's face"
+                />
               </div>
             </div>
           </div>
 
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map((item) => (
-                <a
+              {navigation.map((item, index) => {
+                return <a
                   key={item.name}
                   href={item.href}
                   className={classNames(
@@ -154,10 +103,11 @@ export default function Navbar(): ReactElement {
                     'block px-3 py-2 rounded-md text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
+                  onClick={() => setNavigationCurrent(index)}
                 >
                   {item.name}
-                </a>
-              ))}
+                </a>;
+              })}
             </div>
           </Disclosure.Panel>
         </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,40 @@ import React, { ReactElement } from 'react';
 
 import Navbar from '../components/navbar';
 import Overlay from '../components/overlay';
-import {MyProvider} from '../context/stateProvider';
+import { MyProvider } from '../context/stateProvider';
 
 export default function Home(): ReactElement {
   return (
     <MyProvider>
       <Navbar />
       <Overlay />
-      <div className='w-full h-full flex flex-col items-center justify-center'>
-        <p>my first paragraph</p>
-        <p>my second paragraph</p>
+      <div className="flex justify-center">
+        <div className="container px-5">
+          <section className="mt-10">
+            <h1 className="m-5 text-gray-500 text-4xl">About Me</h1>
+            <p className="m-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo libero quo ea laudantium velit rem dignissimos, voluptatum voluptatem deleniti, facere atque, odit neque laboriosam? Voluptatibus veniam illum harum nam accusantium!</p>
+            <p className="m-5">Unde quod, sequi explicabo ipsam odio voluptate earum repudiandae dignissimos. Blanditiis, animi! Qui fugiat placeat odit repellat accusamus sit tenetur, corporis numquam voluptatum molestias natus mollitia consectetur magnam, maxime cupiditate?</p>
+            <p className="m-5">Nam ratione eaque eveniet facilis ipsum voluptates porro obcaecati alias, totam dolore minima officiis vero eum unde quidem fugit ex, esse exercitationem earum? Modi quia nisi fugit eligendi, aliquam voluptate.</p>
+            <p className="m-5">Et veritatis nihil expedita cumque quia praesentium consequatur debitis cupiditate, vitae architecto eaque, quis corporis harum blanditiis, consequuntur eligendi ut quos in aliquid labore modi? Aut eum ullam officia suscipit.</p>
+            <p className="m-5">Suscipit quo, nostrum, error temporibus similique praesentium repudiandae incidunt a natus ratione sed ea, illo fuga distinctio voluptatem nemo rerum illum unde? Accusantium aperiam nulla voluptate, est nemo sequi voluptatum?</p>
+          </section>
+          <section className="mt-10">
+            <h1 className="m-5 text-gray-500 text-4xl">Portfolio</h1>
+            <p className="m-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo libero quo ea laudantium velit rem dignissimos, voluptatum voluptatem deleniti, facere atque, odit neque laboriosam? Voluptatibus veniam illum harum nam accusantium!</p>
+            <p className="m-5">Unde quod, sequi explicabo ipsam odio voluptate earum repudiandae dignissimos. Blanditiis, animi! Qui fugiat placeat odit repellat accusamus sit tenetur, corporis numquam voluptatum molestias natus mollitia consectetur magnam, maxime cupiditate?</p>
+            <p className="m-5">Nam ratione eaque eveniet facilis ipsum voluptates porro obcaecati alias, totam dolore minima officiis vero eum unde quidem fugit ex, esse exercitationem earum? Modi quia nisi fugit eligendi, aliquam voluptate.</p>
+            <p className="m-5">Et veritatis nihil expedita cumque quia praesentium consequatur debitis cupiditate, vitae architecto eaque, quis corporis harum blanditiis, consequuntur eligendi ut quos in aliquid labore modi? Aut eum ullam officia suscipit.</p>
+            <p className="m-5">Suscipit quo, nostrum, error temporibus similique praesentium repudiandae incidunt a natus ratione sed ea, illo fuga distinctio voluptatem nemo rerum illum unde? Accusantium aperiam nulla voluptate, est nemo sequi voluptatum?</p>
+          </section>
+          <section className="my-10">
+            <h1 className="m-5 text-gray-500 text-4xl">Contact Me</h1>
+            <p className="m-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo libero quo ea laudantium velit rem dignissimos, voluptatum voluptatem deleniti, facere atque, odit neque laboriosam? Voluptatibus veniam illum harum nam accusantium!</p>
+            <p className="m-5">Unde quod, sequi explicabo ipsam odio voluptate earum repudiandae dignissimos. Blanditiis, animi! Qui fugiat placeat odit repellat accusamus sit tenetur, corporis numquam voluptatum molestias natus mollitia consectetur magnam, maxime cupiditate?</p>
+            <p className="m-5">Nam ratione eaque eveniet facilis ipsum voluptates porro obcaecati alias, totam dolore minima officiis vero eum unde quidem fugit ex, esse exercitationem earum? Modi quia nisi fugit eligendi, aliquam voluptate.</p>
+            <p className="m-5">Et veritatis nihil expedita cumque quia praesentium consequatur debitis cupiditate, vitae architecto eaque, quis corporis harum blanditiis, consequuntur eligendi ut quos in aliquid labore modi? Aut eum ullam officia suscipit.</p>
+            <p className="m-5">Suscipit quo, nostrum, error temporibus similique praesentium repudiandae incidunt a natus ratione sed ea, illo fuga distinctio voluptatem nemo rerum illum unde? Accusantium aperiam nulla voluptate, est nemo sequi voluptatum?</p>
+          </section>
+        </div>
       </div>
     </MyProvider>
   );
